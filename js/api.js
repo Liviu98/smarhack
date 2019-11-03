27e1a9737ea7871f21b5f985e355c279132da1b0
Gabriel Matei <gabrielvmatei@gmail.com>
2019-11-03 02:46:53 +0200

    creare si afisare locuri parcare

--- js/api.js
+++ js/api.js
@@ -27,7 +27,8 @@ function callEndpoint(route, type, data, callbackSuccess, callbackError) {
         },
         complete: function(xhr, textStatus) {
             console.log(xhr.status);
-            callbackError(xhr.status);
+            if (xhr.status > 299)
+                callbackError(xhr.status);
             hideLoading();
         }
     }).done(function(html) {
